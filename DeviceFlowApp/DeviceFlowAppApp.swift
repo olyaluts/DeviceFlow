6d26ce9a810d9ef01bc8f1df725f04807ea18e75
Olga Lutsyk <olyalutsyk@gmail.com>
2025-08-06 12:56:26 +0300

    Initial Commit

---
+++ DeviceFlowApp/DeviceFlowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceFlowAppApp.swift
+//  DeviceFlowApp
+//
+//  Created by Olya Lutsyk on 06.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct DeviceFlowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DeviceFlowAppTests/DeviceFlowAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceFlowAppTests.swift
+//  DeviceFlowAppTests
+//
+//  Created by Olya Lutsyk on 06.08.2025.
+//
+
+import Testing
+@testable import DeviceFlowApp
+
+struct DeviceFlowAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
